c6b30412b25440754ccd4b90e4e9646778b9b117
Shivam Gupta <108524973+ashokasec@users.noreply.github.com>
2024-07-15 19:40:35 +0530

    Update route.tsx

--- src/app/contact/route.tsx
+++ src/app/contact/route.tsx
@@ -2,5 +2,5 @@ import { NextResponse } from "next/server";
 const phone_number: string = process.env.NUMBER || ""
 
 export async function GET(request: Request) {
-  return NextResponse.redirect(`https://wa.me/${phone_number}`, 302)
+  return NextResponse.json({message: "This feature is no longer available, as Shivam has stopped it. Contact him on Telegram at https://t.me/ashokasec"})
 }
