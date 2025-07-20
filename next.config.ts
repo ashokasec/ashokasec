ae92ff66545504ff8675fc212d371fd3f9d0887b
ashoka <108524973+ashokasec@users.noreply.github.com>
2025-07-20 21:14:33 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   async redirects() {
         return [
             {
-                source: 'resume',
+                source: '/resume',
                 destination: 'https://drive.google.com/file/d/1aq-f3ucT34IzP04o1fs_xwTwNCzvlDzc/view',
                 permanent: false,
             }
